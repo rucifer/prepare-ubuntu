74a0856eeb673e7cbaed3488e3469ea9bc78f9f9
rucifer <rucifer@163.com>
2015-04-08 15:39:26 +0800

    add usable apt source repository

---
+++ prepare-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+sudo sed -i 's/^deb/#deb/g' /etc/apt/sources.list
+sudo tee /etc/apt/sources.list.d/local.list
+deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu/ trusty-security main restricted universe multiverse
+#deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse
+#deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse
+EOF
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y vim git

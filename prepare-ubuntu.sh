cea0d4c19532ca2aa31bc9db533ce67d7af5fb25
rucifer <rucifer@163.com>
2015-04-09 09:32:59 +0800

    solve EOF issue

--- prepare-ubuntu.sh
+++ prepare-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 sudo sed -i 's/^deb/#deb/g' /etc/apt/sources.list
-sudo tee /etc/apt/sources.list.d/local.list
+sudo tee /etc/apt/sources.list.d/local.list <<EOF
 deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse
 deb http://security.ubuntu.com/ubuntu/ trusty-security main restricted universe multiverse
 #deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted universe multiverse
@@ -8,4 +8,4 @@ deb http://security.ubuntu.com/ubuntu/ trusty-security main restricted universe 
 EOF
 
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y vim git
+sudo apt-get install -y vim openssh-server
